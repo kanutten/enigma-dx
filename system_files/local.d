9cfdf1fd86bc7b8e875f0f957639581746275cbc
kanutten <44906532+kanutten@users.noreply.github.com>
2025-03-16 21:03:37 +0100

    system

---
+++ system_files/local.d
@@ -0,0 +1,25 @@
+[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom5]
+binding='<Shift><Super>s'
+command='flatpak run --command=flameshot org.flameshot.Flameshot gui'
+name='flameshot'
+
+[org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom6]
+binding='<Shift><Control>s'
+command='flatpak run --command=flameshot org.flameshot.Flameshot gui'
+name='flameshot alt'
+
+[org/gtk/settings/file-chooser]
+clock-format='24h'
+
+[system/locale]
+region='nb_NO.UTF-8'
+
+[org/gnome/desktop/calendar]
+show-weekdate=true
+
+[org/gnome/shell/extensions/weather-oclock]
+weather-after-clock=true
+
+[org/gnome/shell/weather]
+automatic-location=true
+locations=[<(uint32 2, <('Haugesund Airport, Karmoy', 'ENHD', false, [(1.0358529110586345, 0.091048015130135024)], @a(dd) [])>)>]
